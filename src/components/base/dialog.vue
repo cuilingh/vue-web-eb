f44702666f4b392066d1f56d9b414c85a678cfc0
cuilingh <984893544@qq.com>
2017-08-19 21:58:10 +0800

    addorderlist

---
+++ src/components/base/dialog.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="dialog-wrap" >
+      <div class="dialog-cover" v-if="isShow"></div>
+      <transition name="drop">
+        <div class="dialog-content" v-if="isShow" >
+          <p class="dialog-close" @click="closeMyself">x</p>
+          <slot>empty</slot><!-- //插槽,保存信息 -->
+        </div>
+      </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isShow: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    closeMyself () {
+      this.$emit('on-close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+/*drop进入快点*/
+.drop-enter-active {
+  transition: all .5s ease;
+}
+drop退出慢点
+.drop-leave-active {
+  transition: all .3s ease;
+}
+.drop-enter {
+  transform: translateY(-500px);
+}
+.drop-leave-active {
+  transform: translateY(-500px);
+}
+
+.dialog-wrap {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
+.dialog-cover {
+  background: #000;
+  opacity: .3;
+  position: fixed;
+  z-index: 5;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.dialog-content {
+  width: 50%;
+  position: fixed;
+  max-height: 50%;
+  overflow: auto;
+  background: #fff;
+  top: 20%;
+  left: 50%;
+  margin-left: -25%;/*//居中*/
+  z-index: 10;
+  border: 2px solid #464068;
+  padding: 2%;
+  line-height: 1.6;
+}
+.dialog-close {
+  position: absolute;
+  right: 5px;
+  top: 5px;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  cursor: pointer;
+}
+.dialog-close:hover {
+  color: #4fc08d;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import VueRouter from 'vue-router'  //./router'./router' //????
 import Layout from './components/layout'
 import IndexPage from './pages/index'
 import VueResource from 'vue-resource'
+import DetailPage from './pages/detail'
+import DetailAnalysisPage from './pages/detail/analysis'
+import DetailCountPage from './pages/detail/count'
+import DetailForecastPage from './pages/detail/forecast'
+import DetailPublishPage from './pages/detail/publish'
 
 // import Banana from './components/banana'
 
@@ -16,10 +21,35 @@ Vue.use(VueResource)//在所有的页面就可以使用this.$http
 //实例化路由
 let router=new VueRouter({
 	mode:'history',
-	routes:[{
-		path:'/', //根目录
-		component:IndexPage //主页面,!!!注意component不加s
-	}]
+	routes:[
+		{
+			path:'/', //根目录
+			component:IndexPage //主页面,!!!注意component不加s
+		},
+		{
+			path:'/detail',
+			component:DetailPage,
+			redirect:'/detail/analysis',//不能直接访问detail页面，跳转到analysis
+			children:[
+				{
+					path:'analysis',//不需要加/
+					component:DetailAnalysisPage
+				},
+				{
+					path:'count',//不需要加/
+					component:DetailCountPage
+				},
+				{
+					path:'forecast',//不需要加/
+					component:DetailForecastPage
+				},
+				{
+					path:'publish',//不需要加/
+					component:DetailPublishPage
+				}
+			]
+		}
+	]
 });
 
 
